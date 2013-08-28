f11e9575c482568cf9ed0d4b93cafe6c98f32af6
Matt Riley <mrileyaz@gmail.com>
2013-08-28 14:29:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# WHS Class of 1996 Page
+
+This is the code used to build the Washington High School Class of 1996 of Phoenix, AZ reunion page.
---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// ios add2home script config
+var addToHomeConfig = {
+	animationIn: 'bubble',
+	animationOut: 'drop',
+	expire: 1,
+	touchIcon: true
+};
+
+(function ($) {
+	"use strict";
+	$('#myCarousel').carousel({
+		interval: 7000
+	});
+})(jQuery);
